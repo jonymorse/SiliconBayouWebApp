eefaba8bf9e5f0e149c342fc4460909287582e72
JM <jomo@MacBook-Pro-98.local>
2025-09-12 07:32:10 -0400

    update lens id

--- proper.js
+++ proper.js
@@ -2,7 +2,7 @@ import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from '@snap/
 
 const LENS_CONFIG = {
     API_TOKEN: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzU2MDg0MjEwLCJzdWIiOiJmODFlYmJhMC1iZWIwLTRjZjItOWJlMC03MzVhMTJkNGQxMWR-U1RBR0lOR345ZWY5YTc2Mi0zMTIwLTRiOTQtOTUwMy04NWFmZjc0MWU5YmIifQ.UR2iAXnhuNEOmPuk7-qsu8vD09mrRio3vNtUo0BNz8M',
-    LENS_ID: '27ae68a3-e7eb-4edb-bf9f-c2880a1a92f2',
+    LENS_ID: '96460356-a721-446e-873a-5c5a2140ed20',
     LENS_GROUP_ID: '1d5338a5-2299-44e8-b41d-e69573824971'
 };
 
